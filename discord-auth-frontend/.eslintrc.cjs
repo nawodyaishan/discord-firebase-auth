b531c52ba412ed8ae3a3c9cc49b95ba9f4c3eaea
Nawodya Ishan <nawodyain@gmail.com>
2024-04-22 21:25:43 +0530

    chore: refactor

--- discord-auth-frontend/.eslintrc.cjs
+++ discord-auth-frontend/.eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -12,7 +13,12 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
+      { allowConstantExport: true }
+    ]
   },
-}
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
